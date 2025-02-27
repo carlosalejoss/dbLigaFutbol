98c9d2e3f5dfdb897b571cac97b34524355a0b52
carlosalejoss <872342@unizar.es>
2025-02-27 19:34:02 +0100

    consulta3 falta por revisar

---
+++ sql/consulta3.sql
@@ -0,0 +1,21 @@
+WITH ultima_temporada_segunda AS (
+    SELECT idTemporada, agno
+    FROM TEMPORADA t1
+    WHERE division = '2'
+    AND agno = (SELECT MAX(t2.agno) FROM TEMPORADA t2 WHERE t2.division = '2')
+),
+goles_por_jornada AS (
+    SELECT j.idJornada, j.temporada, SUM(p.golesLocal + p.golesVisitante) AS total_goles
+    FROM PARTIDO p
+    JOIN JORNADA j ON p.jornada = j.idJornada
+    WHERE j.temporada IN (SELECT idTemporada FROM ultima_temporada_segunda)
+    GROUP BY j.idJornada, j.temporada
+),
+jornada_max_goles AS (
+    SELECT temporada, idJornada, total_goles
+    FROM goles_por_jornada
+    WHERE total_goles = (SELECT MAX(g.total_goles) FROM goles_por_jornada g)
+)
+SELECT t.agno, j.idJornada, j.total_goles
+FROM jornada_max_goles j
+JOIN TEMPORADA t ON j.temporada = t.idTemporada;
