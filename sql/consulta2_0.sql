f11e9c485e43b09ec678517876b80f1598fd905c
carlosalejoss <872342@unizar.es>
2025-02-27 18:41:36 +0100

    consulta2 más corta que comprueba empates en el primer puesto (no muestra el sevilla)

--- sql/consulta2_0.sql
+++ sql/consulta2_0.sql
@@ -8,22 +8,34 @@ equipos_estadios AS (
     FROM EQUIPO e
     JOIN estadios_pequenos ep ON e.estadio = ep.nombreEstadio
 ),
-posiciones_exitosas AS (
-    SELECT c.equipo
+puntajes AS (
+    SELECT c.temporada, c.equipo, c.puntos,
+           (SELECT MAX(c2.puntos) 
+            FROM contiene c2 
+            WHERE c2.temporada = c.temporada) AS max_puntos,
+           (SELECT COUNT(*) 
+            FROM contiene c3 
+            WHERE c3.temporada = c.temporada 
+              AND c3.puntos = (SELECT MAX(c4.puntos) 
+                               FROM contiene c4 
+                               WHERE c4.temporada = c3.temporada)) AS num_lideres
     FROM contiene c
-    JOIN TEMPORADA t ON c.temporada = t.idTemporada
-    WHERE c.puntos IN (
-        SELECT MAX(c2.puntos) 
-        FROM contiene c2 
-        WHERE c2.temporada = c.temporada
-        UNION
-        SELECT MAX(c3.puntos) 
-        FROM contiene c3 
-        WHERE c3.temporada = c.temporada 
-          AND c3.puntos < (SELECT MAX(c4.puntos) FROM contiene c4 WHERE c4.temporada = c3.temporada)
-    )
-    AND t.division = '1'
+),
+posiciones_exitosas AS (
+    SELECT p.temporada, p.equipo
+    FROM puntajes p
+    WHERE p.puntos = p.max_puntos
+       OR (p.num_lideres = 1 
+           AND p.puntos = (SELECT MAX(p2.puntos) 
+                           FROM puntajes p2 
+                           WHERE p2.temporada = p.temporada AND p2.puntos < p.max_puntos))
+),
+equipos_finales AS (
+    SELECT DISTINCT pe.equipo
+    FROM posiciones_exitosas pe
+    JOIN TEMPORADA t ON pe.temporada = t.idTemporada
+    WHERE t.division = '1'
 )
-SELECT DISTINCT ee.nombreOficial
+SELECT ee.nombreOficial
 FROM equipos_estadios ee
-JOIN posiciones_exitosas pe ON ee.nombreOficial = pe.equipo;
+JOIN equipos_finales ef ON ee.nombreOficial = ef.equipo;
