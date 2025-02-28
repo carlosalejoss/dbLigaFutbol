78f1b3403f0d4a5557949b3dd862ca1f7b05fd42
carlosalejoss <872342@unizar.es>
2025-02-28 12:13:51 +0100

    cambio en consulta3 para que se muestre el número de la jornada y no el id

--- sql/consulta3.sql
+++ sql/consulta3.sql
@@ -5,17 +5,17 @@ WITH ultima_temporada_segunda AS (
     AND agno = (SELECT MAX(t2.agno) FROM TEMPORADA t2 WHERE t2.division = '2')
 ),
 goles_por_jornada AS (
-    SELECT j.idJornada, j.temporada, SUM(p.golesLocal + p.golesVisitante) AS total_goles
+    SELECT j.idJornada, j.numero AS numero_jornada, j.temporada, SUM(p.golesLocal + p.golesVisitante) AS total_goles
     FROM PARTIDO p
     JOIN JORNADA j ON p.jornada = j.idJornada
     WHERE j.temporada IN (SELECT idTemporada FROM ultima_temporada_segunda)
-    GROUP BY j.idJornada, j.temporada
+    GROUP BY j.idJornada, j.numero, j.temporada
 ),
 jornada_max_goles AS (
-    SELECT temporada, idJornada, total_goles
+    SELECT temporada, numero_jornada, total_goles
     FROM goles_por_jornada
     WHERE total_goles = (SELECT MAX(g.total_goles) FROM goles_por_jornada g)
 )
-SELECT t.agno, j.idJornada, j.total_goles
+SELECT t.agno, j.numero_jornada, j.total_goles
 FROM jornada_max_goles j
 JOIN TEMPORADA t ON j.temporada = t.idTemporada;
