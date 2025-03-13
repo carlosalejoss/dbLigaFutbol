3dc301c8de47b047701e63375ca802242a0fd2a8
carlosalejoss <872342@unizar.es>
2025-03-13 12:27:34 +0100

    poblarpuntos sin CASE

--- sql/poblarPuntos.sql
+++ sql/poblarPuntos.sql
@@ -2,25 +2,41 @@ UPDATE CONTIENE c
 SET c.puntos = (
     SELECT SUM(puntos)
     FROM (
-        SELECT p.equipoLocal AS equipo, t.idTemporada AS temporada,
-            CASE 
-                WHEN p.golesLocal > p.golesVisitante THEN 3  -- Victoria local
-                WHEN p.golesLocal = p.golesVisitante THEN 1  -- Empate
-                ELSE 0  -- Derrota local
-            END AS puntos
+        SELECT p.equipoLocal AS equipo, t.idTemporada AS temporada, 3 AS puntos
         FROM PARTIDO p
         JOIN JORNADA j ON p.jornada = j.idJornada
         JOIN TEMPORADA t ON j.temporada = t.idTemporada
+        WHERE p.golesLocal > p.golesVisitante
         UNION ALL
-        SELECT p.equipoVisitante AS equipo, t.idTemporada AS temporada,
-            CASE 
-                WHEN p.golesVisitante > p.golesLocal THEN 3  -- Victoria visitante
-                WHEN p.golesVisitante = p.golesLocal THEN 1  -- Empate
-                ELSE 0  -- Derrota visitante
-            END AS puntos
+        SELECT p.equipoLocal AS equipo, t.idTemporada AS temporada, 1 AS puntos
         FROM PARTIDO p
         JOIN JORNADA j ON p.jornada = j.idJornada
         JOIN TEMPORADA t ON j.temporada = t.idTemporada
+        WHERE p.golesLocal = p.golesVisitante
+        UNION ALL
+        SELECT p.equipoVisitante AS equipo, t.idTemporada AS temporada, 1 AS puntos
+        FROM PARTIDO p
+        JOIN JORNADA j ON p.jornada = j.idJornada
+        JOIN TEMPORADA t ON j.temporada = t.idTemporada
+        WHERE p.golesVisitante = p.golesLocal
+        UNION ALL
+        SELECT p.equipoVisitante AS equipo, t.idTemporada AS temporada, 3 AS puntos
+        FROM PARTIDO p
+        JOIN JORNADA j ON p.jornada = j.idJornada
+        JOIN TEMPORADA t ON j.temporada = t.idTemporada
+        WHERE p.golesVisitante > p.golesLocal
+        UNION ALL
+        SELECT p.equipoLocal AS equipo, t.idTemporada AS temporada, 0 AS puntos
+        FROM PARTIDO p
+        JOIN JORNADA j ON p.jornada = j.idJornada
+        JOIN TEMPORADA t ON j.temporada = t.idTemporada
+        WHERE p.golesLocal < p.golesVisitante
+        UNION ALL
+        SELECT p.equipoVisitante AS equipo, t.idTemporada AS temporada, 0 AS puntos
+        FROM PARTIDO p
+        JOIN JORNADA j ON p.jornada = j.idJornada
+        JOIN TEMPORADA t ON j.temporada = t.idTemporada
+        WHERE p.golesVisitante < p.golesLocal
     ) puntos_totales
     WHERE puntos_totales.equipo = c.equipo
     AND puntos_totales.temporada = c.temporada
